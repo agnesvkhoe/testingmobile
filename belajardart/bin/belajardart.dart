fea4123a443bb62d3bf081a42ba767d808cd5994
agnesvkhoe <agnesvkhoe@gmail.com>
2023-09-18 15:07:53 +0700

    komit agnes

---
+++ belajardart/bin/belajardart.dart
@@ -0,0 +1,5 @@
+import 'package:belajardart/belajardart.dart' as belajardart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajardart.calculate()}!');
+}
---
+++ belajardart/test/belajardart_test.dart
@@ -0,0 +1,8 @@
+import 'package:belajardart/belajardart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
